0fc5708adb79f2fe9a7820418f4cffe061094e39
cyberpatrolunit <bplace00@gmail.com>
2023-12-13 14:54:20 -0800

    added spline BG

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { SideBar } from "../nav/SideBar";
 import { Hero } from "./hero/Hero";
 import styles from "./home.module.scss";
@@ -6,8 +6,21 @@ import { Heading } from "../nav/Heading";
 import { About } from "./about/About";
 import { Projects } from "./projects/Projects";
 import { Contact } from "./contact/Contact";
-import { ScrollTop } from "../buttons/ScrollTop"
+import { ScrollTop } from "../buttons/ScrollTop";
+import { Application } from '@splinetool/runtime'; // Import Splinetool Application
+
 export const Home = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null); // Create a ref for the canvas element
+
+  useEffect(() => {
+    // This code will run when the component mounts
+    const canvas = canvasRef.current; // Access the canvas element from the ref
+    if (canvas) {
+      const app = new Application(canvas); // Use the canvas element of type HTMLCanvasElement
+      app.load('https://prod.spline.design/qQGEiViy9RKI4ueL/scene.splinecode');
+    }
+  }, []); // Empty dependency array ensures this runs only once when the component mounts
+
   return (
     <>
       <div className={styles.home}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
+import dynamic from 'next/dynamic';
 import { Home } from "@/components/home/Home";
+
+// Dynamically import the SplineBG component with SSR disabled
+import SplineBG from '@/components/bg/SplineBG';
 
 export default function home() {
   return (
@@ -10,6 +14,7 @@ export default function home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/shivalogo.ico" />
       </Head>
+      <SplineBG />
       <Home />
     </>
   );
