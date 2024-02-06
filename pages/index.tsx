0c5ebff05762de7086250a383cac4b2bfbade1a7
cyberpatrolunit <bplace00@gmail.com>
2024-02-05 20:06:54 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from "next/head";
-// import dynamic from 'next/dynamic';
-import { Home } from "@/components/home/Home";
+import { Home as ImportedHome } from "@/components/home/Home";
 import { Analytics } from '@vercel/analytics/react';
 
-export default function home() {
+export default function Home() {
   return (
     <>
       <Head>
@@ -11,6 +10,24 @@ export default function home() {
         <meta name="description" content="I am a creative technologist with 13 years of delivering exceptional installations." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/bp_logo.ico" />
+        <link rel="preload" href="/path/to/important/style/or/script.css" as="style" /> {/* Adjust the path to your critical CSS or JS */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "http://schema.org",
+            "@type": "Person",
+            "name": "Bryant Place",
+            "jobTitle": "Creative Technologist",
+            "url": "https://www.bryantplace.com",
+            "sameAs": [
+              // Social profiles here
+              "http://www.facebook.com/your-profile",
+              "http://instagram.com/yourProfile",
+              "http://www.linkedin.com/in/your-profile",
+              "http://twitter.com/yourProfile"
+            ]
+            // Add any other relevant schema properties here
+          })}
+        </script>
       </Head>      
       <Home />
       <Analytics />
